879e4a82cd8bb3339744e0907ab0567c8a120116
Mikail <mikailrahman1@hotmail.com>
2025-05-15 23:28:03 -0400

    checkpoint

--- src/ExampleContract.sol
+++ src/ExampleContract.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "src/RulesEngineIntegration.sol";
 /**
  * An example contract that does nothing.
  * This contract is used to demonstrate the use of the `transfer` function.
  */
 
-contract ExampleContract {
-    function transfer(address to, uint256 value) public returns (bool) {
+contract ExampleContract is RulesEngineClientCustom {
+    function transfer(address to, uint256 value) public checkRulesBeforetransfer(to, value) returns (bool) {
         // this function is purposefully empty
         return true;
     }
